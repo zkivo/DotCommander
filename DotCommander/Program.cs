7290b27ae2554a1a63157821415a2169c802a18a
Marco Schivo <marcoschivo1@gmail.com>
2022-05-19 23:20:43 +0200

    primi esperimenti

--- DotCommander/Program.cs
+++ DotCommander/Program.cs
@@ -1,12 +1,47 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine(Console.BackgroundColor);
-Console.WriteLine(Console.BufferHeight);
-Console.WriteLine(Console.BufferWidth);
-Console.WriteLine(Console.WindowHeight);
-Console.WriteLine(Console.WindowWidth);
-Thread.Sleep(3000);
-Console.WriteLine(Console.BufferHeight);
-Console.WriteLine(Console.BufferWidth);
-Console.WriteLine(Console.WindowHeight);
-Console.WriteLine(Console.WindowWidth);
+//Console.WriteLine(Console.BackgroundColor);
+//Console.WriteLine(Console.WindowHeight);
+//Console.WriteLine(Console.WindowWidth);
+Console.Clear();
+
+string[] files = Directory.GetFiles("G:\\Il mio Drive\\Università");
+string[] dirs = Directory.GetDirectories("G:\\Il mio Drive\\Università");
+
+Console.ForegroundColor = (ConsoleColor) 3;
+
+foreach (string file in files)
+{
+    Console.WriteLine(file);
+}
+
+Console.ForegroundColor = (ConsoleColor) 2;
+
+foreach (string a in dirs)
+{
+    Console.WriteLine(a);
+}
+box();
+Console.ReadKey();
+
+void box()
+{
+    for (int i = 0; i < Console.WindowHeight; i++)
+    {
+        for (int j = 0; j < Console.WindowWidth; j++)
+        {
+            if (i == 0 || i == Console.WindowHeight - 1)
+            {
+                Console.Write("\u2500");
+            } else
+            {
+                if (j == 0 || j == Console.WindowWidth - 1)
+                {
+                    Console.Write("\u2502");
+                } else
+                {
+                    Console.Write(" ");
+                }
+            }
+        }
+    }
+}
