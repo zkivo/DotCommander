99b9d5dbb5e16785ce78158fc6a09156b1fbad00
PC <PC@DESKTOP-OAO6F7H>
2022-05-19 22:33:06 +0200

    Aggiungere i file di progetto.

---
+++ DotCommander/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine(Console.BackgroundColor);
+Console.WriteLine(Console.BufferHeight);
+Console.WriteLine(Console.BufferWidth);
+Console.WriteLine(Console.WindowHeight);
+Console.WriteLine(Console.WindowWidth);
+Thread.Sleep(3000);
+Console.WriteLine(Console.BufferHeight);
+Console.WriteLine(Console.BufferWidth);
+Console.WriteLine(Console.WindowHeight);
+Console.WriteLine(Console.WindowWidth);
