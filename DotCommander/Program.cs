1c58a9342edcbdc8a4463d3ab55d723a9e1e4dd2
marco.schivo <marco.schivo@outlook.com>
2022-05-23 16:50:54 +0200

    trying to switch style when pressing arrow keys but the foreground and bg color is a propriety of the console

--- DotCommander/Program.cs
+++ DotCommander/Program.cs
@@ -3,6 +3,7 @@
 //Console.WriteLine(Console.WindowHeight);
 //Console.WriteLine(Console.WindowWidth);
 
+using System;
 using System.Diagnostics;
 
 string opened_directory = "G:\\Il mio Drive\\Università";
@@ -10,15 +11,15 @@ int index_list = 0;
 ConsoleKeyInfo   key_info;
 ConsoleModifiers mod;
 
-Console.BufferHeight = Console.WindowHeight;
-Console.BufferWidth  = Console.WindowWidth;
+Console.BufferHeight  = Console.WindowHeight + 1; // one line is used as a buffer 
+Console.BufferWidth   = Console.WindowWidth;
+Console.CursorVisible = false;
 
 string[] files = Directory.GetFiles(opened_directory);
 string[] dirs = Directory.GetDirectories(opened_directory);
 string[] split;
-
+refresh();
 do {
-    box();
     key_info = Console.ReadKey(true);
     mod = key_info.Modifiers; // alt, shift, ctrl modifiers information
     if (mod > 0) {
@@ -28,18 +29,42 @@ do {
         // None modifiers have been pressed
         if (key_info.Key.Equals(ConsoleKey.DownArrow)) {
             index_list++;
+            switch_style_lines(index_list, index_list + 1);
         } else if (key_info.Key.Equals(ConsoleKey.UpArrow)) {
             index_list--;
+            switch_style_lines(index_list + 2, index_list + 1);
         } else if (key_info.Key.Equals(ConsoleKey.Enter)){
             Process.Start(new ProcessStartInfo(files[index_list]) { UseShellExecute = true });
         } else {
+            refresh();
             Console.WriteLine("dunno");
         }
     }
 } while (true);
 
+void switch_style_lines(int first, int second) {
+    System.Console.CursorLeft = 0;
+    System.Console.CursorTop  = first;
+    ConsoleColor cf_first  = Console.ForegroundColor;
+    ConsoleColor cb_first  = Console.BackgroundColor;
+    System.Console.CursorTop  = second;
+    ConsoleColor cf_second = Console.ForegroundColor;
+    ConsoleColor cb_second = Console.BackgroundColor;
+    
+    ConsoleColor cf_temp   = cf_second;
+    ConsoleColor cb_temp   = cb_second;
+    
+    Console.CursorTop = second;
+    Console.ForegroundColor = cf_first;
+    Console.Write("ads");
+    //Console.MoveBufferArea(0, first, Console.BufferWidth, 1, 0, Console.BufferHeight - 1);
+    //Console.MoveBufferArea(0, second, Console.BufferWidth, 1, 0, first);
+    //Console.MoveBufferArea(0, Console.BufferHeight - 1, Console.BufferWidth, 1, 0, second);
 
-void box() {
+}
+
+void refresh() {
+    Console.Title = opened_directory + " - Dot-Commander";
     Console.Clear();
     Console.CursorTop  = 0;
     Console.CursorLeft = 0;
@@ -51,7 +76,7 @@ void box() {
     //int index = files.Length % index_list;
     int i = 0;
     foreach (string file in files) {
-        if (i == Console.BufferHeight - 2) {
+        if (i == Console.BufferHeight - 3) {
             return;
         } else if (i == index_list) {
             // this record must be highlighted because the cursor is here
